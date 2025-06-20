47a97099d2e036d3cb7f08c2c8b3c85db2ff98c2
stefan-stanca <stefan.stanca@gmail.com>
2025-06-20 11:00:50 +0300

    test3

--- src/App.tsx
+++ src/App.tsx
@@ -25,34 +25,10 @@ const formSchema = {
   ],
 };
 
-const formSchema2 = {
-  title: "Cat Registration",
-  fields: [
-    { label: "Pet", type: "text", name: "pet", required: true },
-    {
-      label: "Species",
-      type: "select",
-      name: "species",
-      options: ["Cat", "kitten", "bunny"],
-    },
-    { label: "Age", type: "number", name: "age" },
-    { label: "Subscribe", type: "checkbox", name: "subscribe" },
-
-    {
-      label: "Gender",
-      type: "select",
-      name: "gender",
-      required: true,
-      options: ["Male", "Female", "Other"],
-    },
-  ],
-};
-
 function App() {
   return (
     <div className="d-flex gap-5">
       <DynamicForm formProp={formSchema} />
-      <DynamicForm formProp={formSchema2} />
     </div>
   );
 }
